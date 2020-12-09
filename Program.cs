361d19714d082580cbf7df9fac1cca9d9f9487d3
colehoward-nz <74393866+colehoward-nz@users.noreply.github.com>
2020-12-09 21:55:45 +1300

    Initial commit

---
+++ Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SecondProject
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string nameOf = "Cole";
+            Console.WriteLine("My name is " + nameOf);
+            Console.WriteLine("What is your name?");
+            nameOf = Console.ReadLine();
+            Console.WriteLine("Hello " + nameOf);
+        }
+    }
+}
